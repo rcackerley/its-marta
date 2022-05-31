abc352ff1004f155e587844814b914d6b0f00b05
Robby Ackerley <rcackerley@me.com>
2022-05-31 17:19:35 -0400

    Calculate optimal latency for requesting new data
    The logging is restricted to 120 seconds. During off hours, holidays, or weekdays, there might be zero results for an extended period of time. This function looks for the closest arriving train and measures the time against 120 seconds to determine an optimal interval for the next call.

--- tasks/helpers.js
+++ tasks/helpers.js
@@ -9,4 +9,13 @@ function logArrivalTimes(trains) {
   })
 }
 
+function calculateLatency(trains) {
+  let waits = trains
+    .map((train) => parseInt(train.WAITING_SECONDS))
+    .sort((a, b) => a - b)
+  if (waits[0] > 120) return (waits[0] - 120) * 1000
+  return 10000
+}
+
 exports.logArrivalTimes = logArrivalTimes
+exports.calculateLatency = calculateLatency
