89761ccd40ebc74b173686bff525e291bbd32189
Robby Ackerley <rcackerley@me.com>
2022-06-01 11:01:48 -0400

    Fix bug where errors were not being handled and triggering the restart

--- tasks/startPoller.js
+++ tasks/startPoller.js
@@ -6,21 +6,27 @@ function startPoller(milliseconds = 10000) {
   let preferredLatency = milliseconds
 
   var pollApi = function () {
-    try {
-      console.log(
-        `Polling marta api at an interval of ${preferredLatency / 1000} seconds`
-      )
-      fetchTrains().then(({ data: trains }) => {
+    console.log(
+      `Polling marta api at an interval of ${preferredLatency / 1000} seconds`
+    )
+    fetchTrains()
+      .then(({ data: trains }) => {
         preferredLatency = calculateLatency(trains)
         logArrivalTimes(filterTrains(trains))
         setTimeout(pollApi, preferredLatency)
       })
-    } catch (error) {
-      setTimeout(pollApi, preferredLatency)
-    }
+      .catch((error) => {
+        console.error(error)
+        restartPoller(pollApi, preferredLatency)
+      })
   }
 
   return setTimeout(pollApi, preferredLatency)
+}
+
+function restartPoller(pollApi, preferredLatency) {
+  console.log(`Retrying failed job`)
+  setTimeout(pollApi, preferredLatency)
 }
 
 exports.startPoller = startPoller
