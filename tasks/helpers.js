0233db37cdf42be0d7d1009653c67200926b4e7a
Robby Ackerley <rcackerley@me.com>
2022-06-02 08:45:14 -0400

    Fix bug for edge case where next arriving train is less than 10 seconds over the alloted 120 seconds check time
    With the requirement of a minimum query time of 10 seconds, there was an edge case where a train could be 125 seconds away, and then the caclulation would return to query in 5 seconds. This commit handles that and lets it fallback to 10 seconds if that case is encountered.

--- tasks/helpers.js
+++ tasks/helpers.js
@@ -1,5 +1,7 @@
 const fs = require("fs")
 
+const TEN_SECONDS = 10000
+
 function logArrivalTimes(trains) {
   let stream = fs.createWriteStream("out.log", { flags: "a" })
   stream.write(JSON.stringify(trains))
@@ -13,8 +15,13 @@ function calculateLatency(trains) {
   let waits = trains
     .map((train) => parseInt(train.WAITING_SECONDS))
     .sort((a, b) => a - b)
-  if (waits[0] > 120) return (waits[0] - 120) * 1000
-  return 10000
+  if (waits[0] > 120) return determineNextNeededPoll(waits[0])
+  return TEN_SECONDS
+}
+
+function determineNextNeededPoll(closestTrainTime) {
+  if (closestTrainTime - 120 < 10) return TEN_SECONDS
+  return (closestTrainTime - 120) * 1000
 }
 
 exports.logArrivalTimes = logArrivalTimes
